d116dc5274d5770d807bf4f6d9fc32a7af54a718
Gonçalo M. <dnigamerofficial@gmail.com>
2025-05-04 20:04:10 +0100

    Initial commit

---
+++ src/main/java/xyz/dnigamer/surveynotifier/SurveyNotifierApplication.java
@@ -0,0 +1,13 @@
+package xyz.dnigamer.surveynotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyNotifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurveyNotifierApplication.class, args);
+    }
+
+}
